7473179b78c0cbf0bd8b7185dc6e1967568e49c9
SaraElsayed <esara7772@gmail.com>
2025-06-11 21:09:46 +0300

    "addcustom_textformfield"

--- lib/component/custom_textfiled.dart
+++ lib/component/custom_textfiled.dart
@@ -2,22 +2,20 @@ import 'package:flutter/material.dart';
 
 class CustomTextFiled extends StatelessWidget {
 
-  const CustomTextFiled({super.key,required this.textData , this.hintTextData,this.size,this.textcolor,
+ CustomTextFiled({super.key,required this.textData , this.hintTextData,this.size,this.textcolor,
   this.hintcolor,this.validator,required this.controller,this.maxLines
 });
   
-  final String? textData ;
+  final String textData ;
   final String? hintTextData;
   final double? size;
   final int? textcolor ;
   final int? hintcolor;
   final TextEditingController controller;
-  final Function(String?)? validator;
+ final String? Function(String?)?validator;
   final int? maxLines;
 
    
-   
-
   @override
   Widget build(BuildContext context) {
 
@@ -25,10 +23,10 @@ class CustomTextFiled extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-               "$textData",
+               textData,
                style: Theme.of(context).textTheme.titleMedium  
             ),
-
+    
             SizedBox(
               height: 8,
             ),
@@ -40,7 +38,7 @@ class CustomTextFiled extends StatelessWidget {
             decoration: InputDecoration(
               hintText: hintTextData,
             ),
-           
+            validator: validator != null ? (String? value) => validator!(value) : null,
            )
         
           ],
