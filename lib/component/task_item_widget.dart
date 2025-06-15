9a3eea3e4eb06c413d9c85beda57f5eaf2affc6a
SaraElsayed <esara7772@gmail.com>
2025-06-15 09:09:28 +0300

    "edit task_item"

--- lib/component/task_item_widget.dart
+++ lib/component/task_item_widget.dart
@@ -66,7 +66,7 @@ class TaskItemWidget extends StatelessWidget {
           ),
           PopupMenuButton<TaskItemActionsEnum>(
             icon: SvgPicture.asset(
-              'assets/images/dots-vertical.svg',
+              'assets/images/more.svg',
               width: 24,
               height: 24,
             ),
@@ -144,78 +144,80 @@ class TaskItemWidget extends StatelessWidget {
               (BuildContext context, void Function(void Function()) setState) {
             return Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Form(
-                key: key,
-                child: Column(
-                  children: [
-                    SizedBox(height: 20),
-                    CustomTextFiled(
-                      textData: 'Task Name',
-                      hintTextData: 'Finish UI design for login screen',
-                      maxLines: 1,
-                      controller: taskNameController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter a task name';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    CustomTextFiled(
-                      textData: 'Task Description',
-                      hintTextData:
-                          'Finish onboarding UI and hand off to devs by Thursday.',
-                      maxLines: 5,
-                      controller: taskDescriptionController,
-                    ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'High Priority  ',
-                          style: Theme.of(context).textTheme.displaySmall,
-                        ),
-                        Switch(
-                            value: isHighPriority,
-                            onChanged: (bool newValue) {
-                              setState(() {
-                                isHighPriority = newValue;
-                              });
-                            }),
-                      ],
-                    ),
-                    const SizedBox(height: 90),
-                    ElevatedButton.icon(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 56),
-                      ),
-                      icon: const Icon(Icons.edit, size: 18),
-                      onPressed: () async {
-                        if (key.currentState?.validate() ?? false) {
-                          TaskModel newTask = TaskModel(
-                            name: taskNameController.text,
-                            description: taskDescriptionController.text,
-                            isHighPriority: isHighPriority,
-                            id: model.id, // Ensure to pass the existing task ID
-                          );
-
-                          await Provider.of<TasksController>(context,
-                                  listen: false)
-                              .editTask(
-                            newTask,
-                          );
-                          taskNameController.clear();
-                          taskDescriptionController.clear();
-                          if (context.mounted) {
-                            Navigator.pop(context);
+              child: SingleChildScrollView(
+                child: Form(
+                  key: key,
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      CustomTextFiled(
+                        textData: 'Task Name',
+                        hintTextData: 'Finish UI design for login screen',
+                        maxLines: 1,
+                        controller: taskNameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a task name';
                           }
-                        }
-                      },
-                      label: const Text('Edit Task'),
-                    ),
-                  ],
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      CustomTextFiled(
+                        textData: 'Task Description',
+                        hintTextData:
+                            'Finish onboarding UI and hand off to devs by Thursday.',
+                        maxLines: 5,
+                        controller: taskDescriptionController,
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'High Priority  ',
+                            style: Theme.of(context).textTheme.displaySmall,
+                          ),
+                          Switch(
+                              value: isHighPriority,
+                              onChanged: (bool newValue) {
+                                setState(() {
+                                  isHighPriority = newValue;
+                                });
+                              }),
+                        ],
+                      ),
+                      const SizedBox(height: 90),
+                      ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 56),
+                        ),
+                        icon: const Icon(Icons.edit, size: 18),
+                        onPressed: () async {
+                          if (key.currentState?.validate() ?? false) {
+                            TaskModel newTask = TaskModel(
+                              name: taskNameController.text,
+                              description: taskDescriptionController.text,
+                              isHighPriority: isHighPriority,
+                              id: model.id, // Ensure to pass the existing task ID
+                            );
+                
+                            await Provider.of<TasksController>(context,
+                                    listen: false)
+                                .editTask(
+                              newTask,
+                            );
+                            taskNameController.clear();
+                            taskDescriptionController.clear();
+                            if (context.mounted) {
+                              Navigator.pop(context);
+                            }
+                          }
+                        },
+                        label: const Text('Edit Task'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
